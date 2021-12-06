b5b6c55b9d7b240ff5d50b0b53d8df4d08580b94
Anas Al Kala <aal1580@students.fhv.at>
2021-12-06 23:32:29 +0100

    adding a details component and a Video frame

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React from 'react';
 import SearchBar from './Components/SearchBar';
 import youtube from './apis/youtube';
 import VideoList from './Components/VideoList';
+import VideoDetail from './Components/VideoDetail';
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
@@ -16,11 +17,16 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   }
 
+  onSelectVideo = (video) => {
+    this.setState({ selectedVideo: video });
+  }
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmitFromChild={this.onTermSubmit} />
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList videos={this.state.videos} onSelectVideo={this.onSelectVideo} />
       </div>
     );
   }
--- src/Components/VideoList.js
+++ src/Components/VideoList.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onSelectVideo }) => {
     const renderedList = videos.map((video) => {
-        return <VideoItem video={video} />
+        return <VideoItem video={video} key={video.id.videoId} onSelectVideo={onSelectVideo} />
     });
 
     return (
-        <div class="ui relaxed divided list">
+        <div className="ui relaxed divided list">
             {renderedList}
         </div>
     );
